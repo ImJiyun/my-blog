74a11e7e29e36e3f5d0759983a21cef1b36d7e5e
Jiyun <jiyunk920@gmail.com>
2025-04-06 10:03:36 +0900

    ✨  feat: implement pagination

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,5 +1,7 @@
 import { posts } from "#site/content";
 import { MDXContent } from "@/components/mdx-components";
+import { formatDate } from "@/lib/utils";
+import { Calendar } from "lucide-react";
 import { notFound } from "next/navigation";
 
 interface PostDetailPageProps {
@@ -31,11 +33,15 @@ export default async function PostDetailPage({ params }: PostDetailPageProps) {
   }
 
   return (
-    <div className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
+    <div className="container py-6 prose dark:prose-invert max-w-3xl mx-auto items-center gap-3">
       <h1 className="mb-2 text-blue-500 text-2xl sm:text-3xl">{post.title}</h1>
       {post.description ? (
         <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
       ) : null}
+      <div className="flex items-center gap-3">
+        <Calendar className="h-4 w-4" />
+        <time dateTime={post.date}>{formatDate(post.date)}</time>
+      </div>
       <hr className="my-4" />
       <MDXContent code={post.body} />
     </div>
